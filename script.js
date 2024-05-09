f4f0639e2cdff3b2b770147896b647e1ef9e12f2
cleberson <132245900+Clebersonmrs@users.noreply.github.com>
2024-05-09 10:51:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,69 @@
+const list = document.querySelector('.ul')
 const buttonShowAll = document.querySelector('.show-all')
-myLi = ''
+const buttonModifi = document.querySelector('.map-all')
+const buttonReduce = document.querySelector('.reduce-all')
+const filterAll = document.querySelector('.filter-all')
 
-function showAll() {
-    menuOptions.forEach(product => {
-        `
-        <li class = showAll>
-            <img src = product.src>
-            <p>X-Bacon Egg</p>
-            <p class="item-price">R$ 30,00</p>
+function showAll(products) {
+
+    myLi = ''
+
+    products.forEach((product) => {
+        myLi += `
+        <li>
+            <img src=${product.src}>
+            <p>${product.name}</p>
+            <p class="item-price">R$ ${formatCurrency(product.price)}</p>
         </li>
+    
+    
+        `
+    })
 
-`
+    list.innerHTML = myLi
 
+}
+function newValues() {
+    const newPrices = menuOptions.map((product) => ({
+        ...product,
+        price: product.price * 0.9,
 
-    });
+    }))
+
+    showAll((newPrices))
+}
+
+function somarValues() {
+    const sumValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
+
+    list.innerHTML = `
+        <li>
+            <p>O valor total dos itens foi de R$ ${formatCurrency(sumValue)}</p>
+        </li>
+    
+        `
+}
+
+function formatCurrency(value) {
+    const newVlr = value.toLocaleString('pt-br', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+    return newVlr
+}
+
+function filtrando() {
+    const vegans = menuOptions.filter(value => value.vegan === true)
+
+    showAll(vegans)
+
 }
 
 
+buttonShowAll.addEventListener('click', () => showAll(menuOptions))
+buttonModifi.addEventListener('click', newValues)
+buttonReduce.addEventListener('click', somarValues)
+filterAll.addEventListener('click', filtrando)
 
-buttonShowAll.addEventListener('click', showAll)
+
+
