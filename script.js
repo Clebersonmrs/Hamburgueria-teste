c1a3eb09fea4a8a02d67829a951069a8a739f6c6
cleberson <132245900+Clebersonmrs@users.noreply.github.com>
2024-05-16 17:25:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,88 @@
 const list = document.querySelector('.ul')
 const buttonShowAll = document.querySelector('.show-all')
-const buttonModifi = document.querySelector('.map-all')
-const buttonReduce = document.querySelector('.reduce-all')
-const filterAll = document.querySelector('.filter-all')
+const mapAll = document.querySelector('.map-all')
+const reduceAll = document.querySelector('.reduce-all')
+const buttonFilterAll = document.querySelector('.filter-all')
+const buttonDrinkd = document.querySelector('.drink-all')
+const contato = document.querySelector('.contato')
 
-function showAll(products) {
+function novis(arrayProduct) {
 
-    myLi = ''
+  let myLi = ''
 
-    products.forEach((product) => {
-        myLi += `
-        <li>
-            <img src=${product.src}>
-            <p>${product.name}</p>
-            <p class="item-price">R$ ${formatCurrency(product.price)}</p>
-        </li>
-    
-    
-        `
-    })
+  arrayProduct.forEach((product) => {
+    myLi +=
 
+      `
+          <li class = showAll>
+              <img src= ${product.src}>
+              <p>${product.name}</p>
+              <p class="item-price">R$ ${formatacao(product.price)}</p>
+          </li> 
+    `
+  })
+  list.innerHTML = myLi
+}
+
+function drinks() {
+  let myLi = ''
+  drinkOption.forEach(product => {
+    myLi +=
+
+      `
+    <li class = drinkShow>
+    <img src= ${product.src}>
+    <p>${product.name}</p>
+    <p class="item-price">R$ ${formatacao(product.price)}</p>
+</li>
+  `
     list.innerHTML = myLi
-
-}
-function newValues() {
-    const newPrices = menuOptions.map((product) => ({
-        ...product,
-        price: product.price * 0.9,
-
-    }))
-
-    showAll((newPrices))
-}
-
-function somarValues() {
-    const sumValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
-
-    list.innerHTML = `
-        <li>
-            <p>O valor total dos itens foi de R$ ${formatCurrency(sumValue)}</p>
-        </li>
-    
-        `
-}
-
-function formatCurrency(value) {
-    const newVlr = value.toLocaleString('pt-br', {
-        style: 'currency',
-        currency: 'BRL'
-    })
-    return newVlr
-}
-
-function filtrando() {
-    const vegans = menuOptions.filter(value => value.vegan === true)
-
-    showAll(vegans)
+  });
 
 }
 
+function formatacao(value) {
+  const nei = value.toLocaleString('pt-br', {
+    style: 'currency',
+    currency: 'BRL'
+  });
+  return nei
+}
 
-buttonShowAll.addEventListener('click', () => showAll(menuOptions))
-buttonModifi.addEventListener('click', newValues)
-buttonReduce.addEventListener('click', somarValues)
-filterAll.addEventListener('click', filtrando)
+function alteradoPreco() {
+  const newPrice = menuOptions.map((product) => ({
+    ...product,
+    price: product.price * 0.9
+
+  }))
+  novis(newPrice)
+}
+
+
+function Adicionar() {
+  const reduceTotal = menuOptions.reduce((acc, product) => acc + product.price, 0)
+
+  list.innerHTML =
+    `
+  <li>
+      <p>O valor total dos itens foi de R$ ${formatacao(reduceTotal)}</p>
+  </li>
+
+  `
+}
+
+function filtro() {
+
+  const fil = menuPromotion.filter(product => product.promo == true)
+  novis(fil)
+}
 
 
 
+
+
+buttonShowAll.addEventListener('click', () => novis(menuOptions))
+mapAll.addEventListener('click', alteradoPreco)
+reduceAll.addEventListener('click', Adicionar)
+buttonFilterAll.addEventListener('click', filtro)
+buttonDrinkd.addEventListener('click', drinks)
