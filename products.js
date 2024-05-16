4b016f765e96994f481980fb31e346376b7cb8d1
cleberson <132245900+Clebersonmrs@users.noreply.github.com>
2024-05-16 17:25:05 -0300

    Update products.js

--- products.js
+++ products.js
@@ -5,6 +5,27 @@ const menuOptions = [
     { name: 'Monstruoso', price: 50, vegan: false, src: './img/monstruoso.png' },
     { name: 'Big Vegano', price: 55, vegan: true, src: './img/xvegan.png' },
     { name: 'X-Vegan', price: 45, vegan: true, src: './img/monstruoso-vegan.png' },
-    ]
-    
-  
+    { name: 'X-Picanha', price: 60, vegan: true, src: './img/X-Picanha.jpg' },
+    { name: 'CheddBurguer', price: 65, vegan: true, src:'./img/chedar.jpg' },
+
+]
+
+const menuPromotion = [
+    { name: 'Gran Burguer', price: 65, promo: true, src: './img/combo.webp' },
+    { name: 'Trio Burguer', price: 120, promo: true, src: './img/trio.jpg' },
+    { name: 'Combo Casal', price: 75, promo: true, src: './img/combo-casal.png' },
+    { name: 'Chicken Burguer', price: 90, promo: true, src: './img/hambúrguer-de-frango.jpg' },
+]
+
+const drinkOption = [
+    { name: 'Coca-Zero', price: 9, src: "./img/coca-zero.jpg" },
+    { name: 'Guaraná', price: 9, src: "./img/gurana.jpg" },
+    { name: 'Coca-Cola', price: 9, src: "./img/coca.jpg" },
+    { name: 'Sprite', price: 9, src: "./img/sprite.webp" },
+    { name: 'Schwepes', price: 9, src: "./img/citrus.webp" },
+    { name: 'Fanta', price: 9, src: "./img/fanta.jpg" },
+    { name: 'Guaraná-Jesus', price: 9, src: "./img/jesus.jpg" },
+    { name: 'Fanta-Uva', price: 9, src: "./img/fanta-uva.webp" },
+]
+
+
